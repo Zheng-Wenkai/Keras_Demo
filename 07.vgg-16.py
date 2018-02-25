ca6ec2c9411247df234b740862cecac024c10f3d
Zheng_Wenkai <841072130@qq.com>
2018-02-25 15:12:28 +0800

    add decode_predictions

--- 07.vgg-16.py
+++ 07.vgg-16.py
@@ -1,16 +1,17 @@
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import preprocess_input,decode_predictions
 from keras.models import Model
 import numpy as np
 import matplotlib.pyplot as plt
 
-#加载并显示图片
+'''加载并显示图片'''
 img_path = 'elephant.jpg'
 img = image.load_img(img_path, target_size=(224, 224))
 plt.imshow(img)
 plt.show()
 
+'''图片预处理'''
 #将图片转成numpy array
 x = image.img_to_array(img)
 #扩展维度,因为preprocess_input需要4D的格式
@@ -20,24 +21,6 @@ x = preprocess_input(x)
 
 #加载VGG16模型（包含全连接层）
 model = VGG16(include_top=True, weights='imagenet')
-#对模型进行评估，函数返回值是预测值的numpy array
-scores = model.predict(x)
-class_table = open('synset_words.txt', 'r')
-lines = class_table.readlines()
-#找出给定张量最大值的位置
-print(np.argmax(scores))
-print('result is ', lines[np.argmax(scores)])
-class_table.close()
-del model
-
-# 加载VGG16模型（不包含包含全连接层）
-model = VGG16(weights='imagenet', include_top=False)
-features = model.predict(x)
-print(features.shape)
-
-#提取block5_pool层特征
-model_extractfeatures = Model(input=model.input, output=model.get_layer('block5_pool').output)
-block5_pool_features = model_extractfeatures.predict(x)
-feature_image = block5_pool_features[:,:,:,0].reshape(7,7)
-plt.imshow(feature_image)
-plt.show()
+preds = model.predict(x)
+# 将结果解码成一个元组列表（类、描述、概率）（批次中每个样本的一个这样的列表）
+print('Predicted:', decode_predictions(preds, top=3)[0])
